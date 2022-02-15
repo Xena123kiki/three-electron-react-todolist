3d029b0a86b40438d1bfda9d2303df77cdade40a
Xena123kiki <ckdnwls1215@snu.ac.kr>
2022-02-15 17:24:10 +0900

    added todolist

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Todo from './Todo'
 
 import React from 'react';
 
+// Todo: css resizing between todo component and three component
+// Todo: css styling in todo component
+// Todo: active visualization of number of todos in three component
+
 function App() {
   return (
     <div className="App">
